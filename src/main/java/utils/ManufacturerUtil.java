5eaaf3c1e4fabcdfd9d8ae6f798725e8104a71ef
Darragh McGarry <darraghmcgarry877@gmail.com>
2023-11-19 15:16:39 +0000

    DMC - Added Manufacturer scene and controller

---
+++ src/main/java/utils/ManufacturerUtil.java
@@ -0,0 +1,21 @@
+package utils;
+
+public class ManufacturerUtil {
+    public String manufacturer;
+
+    public ManufacturerUtil(String manufacturer){
+        setManufacturer(manufacturer);
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getManufacturer(){
+        return manufacturer;
+    }
+    @Override
+    public String toString(){
+        return "Manufacturer: "+ manufacturer;
+    }
+}
