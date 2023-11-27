e76cd066271f849eb39994f765fc77d018970395
igor <20102236@mail.wit.ie>
2023-11-27 12:44:54 +0000

    fixes

--- src/main/java/Controllers/API.java
+++ src/main/java/Controllers/API.java
@@ -17,6 +17,7 @@ import java.util.List;
 
 
 public class API {
+    public Hashing hashing;
 
     public static <T> void updateListView(String filter, TableView tableView, Node<T> head) {
         Platform.runLater(() -> {
